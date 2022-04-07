ee01b294b24c394682fb9192a3cbdb4f02994c10
whomaxiswellprojects <whomaxiswell.projects@gmail.com>
2022-04-07 14:41:31 +0000

    Init Commit

---
+++ gitexistpush.sh
@@ -0,0 +1,3 @@
+git add *
+git commit -m "Init Commit"
+git push -u origin main
---
+++ gitpush.sh
@@ -0,0 +1,7 @@
+echo "# celctoryJS" >> README.md
+git init
+git add *
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/whomaxiswellprojects/celctoryJS.git
+git push -u origin main
