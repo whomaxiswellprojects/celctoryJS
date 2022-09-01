2d3824248e13a4d032c0d6a960565db1f24a8db1
whomaxiswellprojects <98979338+whomaxiswellprojects@users.noreply.github.com>
2022-09-01 16:32:09 +0300

    Delete gitpush.sh

--- gitpush.sh
+++
@@ -1,7 +0,0 @@
-echo "# celctoryJS" >> README.md
-git init
-git add *
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/whomaxiswellprojects/celctoryJS.git
-git push -u origin main
